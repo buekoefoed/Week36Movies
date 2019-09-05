c75b326d9793263466b48fd1fb115d87bc569a0d
Matias <buekoefoed@gmail.com>
2019-09-05 11:07:11 +0200

    added script.js

---
+++ src/main/webapp/script.js
@@ -0,0 +1,17 @@
+// console.log('Hello from my new project');
+
+// const persons = ['Henrik','Helge','Hans','Holger'];
+const root = document.getElementById('root');
+const btn1 = document.getElementById('btn1');
+const url = "https://buehub.com/rest-jpa-devops-starter/api/movie/all";
+
+btn1.onclick = function(){
+    // console.log('Button was pressed');
+    // // root.innerHTML = persons.map(function(el){return '<li>'+el+'</li>'});
+    // const p = persons.map(el => '<li>' + el + '</li>');
+    // root.innerHTML = '<ul>' + p.join('') + '</ul>';
+    fetch(url)
+    .then(response => response.json())
+    .then(data => root.innerHTML = data.value);
+}
+
